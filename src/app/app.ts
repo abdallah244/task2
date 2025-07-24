8f4d8b9e169259244935771e0c0142d59b422090
abdallah244 <abdallahhfares@gmail.com>
2025-07-24 18:30:57 +0300

    task 2

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,41 @@
+import {
+  CurrencyPipe,
+  DatePipe,
+  UpperCasePipe,
+  NgClass,
+  NgFor,
+  NgIf,
+} from '@angular/common';
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    UpperCasePipe,
+    DatePipe,
+    CurrencyPipe,
+    NgClass,
+    NgFor,
+    NgIf,
+  ],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrls: ['./app.css'],
 })
 export class App {
   protected readonly title = signal('task3');
+
+  name = signal('Abdallah');
+  today = signal(new Date());
+  price = signal(1500);
+
+  items = signal(['cloth', 'accessory', 'any thing']);
+  showPrice = signal(true);
+
+  counter = signal(0);
+
+  increment() {
+    this.counter.update((count) => count + 1);
+  }
 }
